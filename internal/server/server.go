2869adb3d5244100f05d4d27a5de7d09b5a08dfe
Sergio Moya <1083296+smoya@users.noreply.github.com>
2019-06-15 18:30:58 +0200

    Move server to internal package and configure it through env vars (#5)

---
+++ internal/server/server.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"context"
+	"log"
+
+	ratio "github.com/smoya/ratio/api/proto"
+)
+
+// GRPC is a GRPC server.
+type GRPC struct{}
+
+// RateLimit implements ratio.RateLimitService
+func (s *GRPC) RateLimit(ctx context.Context, r *ratio.RateLimitRequest) (*ratio.RateLimitResponse, error) {
+	log.Printf("RateLimit request: %s -> %s\n", r.Owner, r.Resource)
+	return &ratio.RateLimitResponse{
+		Code: ratio.RateLimitResponse_UNKNOWN,
+	}, nil
+}
